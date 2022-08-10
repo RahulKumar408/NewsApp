62599b1d65a5df4a1ab7187f6ee28c17030dddf5
Rahul <kumar.rahul@iitgn.ac.in>
2022-08-11 04:38:18 +0530

    initial commit

---
+++ precache-manifest.d5cef51b200140c48e5044694138f12a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5205fc3bc908e08d6d2",
+    "url": "/static/css/main.9792e263.chunk.css"
+  },
+  {
+    "revision": "e5205fc3bc908e08d6d2",
+    "url": "/static/js/main.e5205fc3.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "d8118e2cb2c02500ae69",
+    "url": "/static/js/2.d8118e2c.chunk.js"
+  },
+  {
+    "revision": "39a7286a869c5b2ec7f4",
+    "url": "/static/js/runtime~main.39a7286a.js"
+  },
+  {
+    "revision": "f627c35e973691c64a3633ecbd222f33",
+    "url": "/index.html"
+  }
+];
